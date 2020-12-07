9e9b7bd3fdc6bd9b09014c619ddd39b7273aedd2
NguyenThanhDuong113bn <nguyenthanhduong113bn@gmail.com>
2020-12-07 22:25:15 +0700

    u

---
+++ Module2/2.Vong_lap_for/src/_06_ke_thua/thuchanh/Geometric.java
@@ -0,0 +1,38 @@
+package _06_ke_thua.thuchanh;
+
+public class Geometric {
+    private String color = "white";
+    private String filled = null;
+
+    /* các khởi tạo tử */
+    public Geometric() {
+        System.out.println("ham khoi tao rong");
+    }
+
+    public Geometric(String color, String filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    /* các thuộc tính */
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getFilled() {
+        return filled;
+    }
+
+    public void setFilled(String filled) {
+        this.filled = filled;
+    }
+
+    public String toString() {
+        return "created with \"" + color + "\" color and " +
+                (filled == null ? "no fill" : "filled with \"" + filled + "\" color");
+    }
+}
